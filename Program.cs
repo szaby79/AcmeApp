03813d9998ab4d6530dd00cb29c7288891221300
Szabolcs Becze <szaby.tile@gmail.com>
2026-05-13 14:06:57 -0400

    Module 12 Console App Assignment

--- Program.cs
+++ Program.cs
@@ -1,54 +1,32 @@
 ﻿using System;
 
-class Program
+namespace ConsoleAppAssignment
 {
-    static void Main()
+    class Program
     {
-        // Welcome message
-        Console.WriteLine("Welcome to Package Express. Please follow the instructions below.");
-
-        // Ask for package weight
-        Console.WriteLine("Please enter the package weight:");
-        int weight = Convert.ToInt32(Console.ReadLine());
-
-        // Check if package is too heavy
-        if (weight > 50)
+        static void Main(string[] args)
         {
-            Console.WriteLine("Package too heavy to be shipped via Package Express. Have a good day.");
-        }
-        else
-        {
-            // Ask for package width
-            Console.WriteLine("Please enter the package width:");
-            int width = Convert.ToInt32(Console.ReadLine());
+            // This is a do while loop
+            int count = 0;
 
-            // Ask for package height
-            Console.WriteLine("Please enter the package height:");
-            int height = Convert.ToInt32(Console.ReadLine());
-
-            // Ask for package length
-            Console.WriteLine("Please enter the package length:");
-            int length = Convert.ToInt32(Console.ReadLine());
-
-            // Add dimensions together
-            int totalDimensions = width + height + length;
-
-            // Check if package is too big
-            if (totalDimensions > 50)
+            do
             {
-                Console.WriteLine("Package too big to be shipped via Package Express.");
+                Console.WriteLine("Inside the loop");
+                count++; // Add 1 to count each time
             }
-            else
+            while (count < 5); // Continue until count reaches 5
+
+
+            // This is a while loop
+            int counting = 0;
+
+            while (counting < 3) // Continue until counting reaches 3
             {
-                // Calculate shipping quote
-                decimal quote = (width * height * length * weight) / 100m;
-
-                // Display quote
-                Console.WriteLine("Your estimated total for shipping this package is: $" + quote);
-                Console.WriteLine("Thank you!");
+                Console.WriteLine("Counting numbers");
+                counting++; // Add 1 each time
             }
-        }
 
-        Console.ReadLine();
+            Console.ReadLine();
+        }
     }
 }
