50b290385e15941d71ab02ece56e325364074cbc
Szabolcs Becze <szaby.tile@gmail.com>
2026-05-22 17:38:24 -0400

    Fixed Method Class Assignment

--- Program.cs
+++ Program.cs
@@ -1,33 +1,19 @@
 ﻿using System;
 
-// Static class
-static class MathHelper
+// Class that contains the math method
+class MathClass
 {
-    // Void method that divides a number by 2
-    public static void DivideByTwo(int number)
+    // Void method that takes two integers as parameters
+    public void MathOperation(int num1, int num2)
     {
-        int result = number / 2;
+        // Perform math operation on the first number
+        int result = num1 + 7;
 
-        // Display result to the screen
-        Console.WriteLine("Result: " + result);
-    }
+        // Display the result of the first number
+        Console.WriteLine("First number plus 7 = " + result);
 
-    // Method with output parameter
-    public static void GetSquare(int number, out int square)
-    {
-        square = number * number;
-    }
-
-    // Overloaded method with one parameter
-    public static int AddNumbers(int number)
-    {
-        return number + 10;
-    }
-
-    // Overloaded method with two parameters
-    public static int AddNumbers(int number1, int number2)
-    {
-        return number1 + number2;
+        // Display the second number
+        Console.WriteLine("Second number = " + num2);
     }
 }
 
@@ -35,33 +21,14 @@ class Program
 {
     static void Main(string[] args)
     {
-        // Ask user to enter a number
-        Console.WriteLine("Enter a number:");
+        // Instantiate the class
+        MathClass math = new MathClass();
 
-        // Convert user input to integer
-        int userNumber = Convert.ToInt32(Console.ReadLine());
+        // Call the method normally
+        math.MathOperation(10, 5);
 
-        // Call void method
-        MathHelper.DivideByTwo(userNumber);
-
-        // Call method with output parameter
-        int squaredNumber;
-        MathHelper.GetSquare(userNumber, out squaredNumber);
-
-        // Display squared number
-        Console.WriteLine("Squared number: " + squaredNumber);
-
-        // Call overloaded method with one parameter
-        int addResult1 = MathHelper.AddNumbers(userNumber);
-
-        // Display result
-        Console.WriteLine("Number plus 10: " + addResult1);
-
-        // Call overloaded method with two parameters
-        int addResult2 = MathHelper.AddNumbers(userNumber, 5);
-
-        // Display result
-        Console.WriteLine("Number plus 5: " + addResult2);
+        // Call the method using named parameters
+        math.MathOperation(num1: 20, num2: 8);
 
         // Keep console window open
         Console.ReadLine();
